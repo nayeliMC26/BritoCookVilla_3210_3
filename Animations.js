206df6957226c966214b5ed9107f3187403b88b9
britomartinw <britomartinw@merrimack.edu>
2024-10-30 18:14:49 -0400

    Cleaned up my code up a little

--- Animations.js
+++ Animations.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three';
-import { color } from 'three/webgpu';
 
 /**
  * Handles all animations
@@ -8,159 +7,107 @@ export class Animations {
 
     constructor(scene) {
         this.scene = scene;
+        // Information to annimate player one
         this.playerOne = {
             flipPath: new THREE.CatmullRomCurve3([
-                new THREE.Vector3(-14, 0.0079, 0),
+                new THREE.Vector3(-14, 0, 0),
                 new THREE.Vector3(-9, 3, 0),
                 new THREE.Vector3(-4, 0, 0)
             ]),
+            // Used to track when the animation stars to loop
             index: -1,
-            stack: 0.0079 / 2,
+            // Tracks the depth o fthe stack location (where is going to end up)
+            stack: 0.01,
+            // Color of line used for debbuging 
             color: 0xff0000
         };
-
+        // Informataion to animate player two
         this.playerTwo = {
             flipPath: new THREE.CatmullRomCurve3([
-                new THREE.Vector3(14, 0.0079, 0),
+                new THREE.Vector3(14, 0, 0),
                 new THREE.Vector3(9, 3, 0),
                 new THREE.Vector3(4, 0, 0)
             ]),
             index: -1,
-            stack: 0.0079 / 2,
+            stack: 0.01,
             color: 0x0000ff
         };
-        // Path to flip the cards of the first player
-        this.flipCardPathOne = new THREE.CatmullRomCurve3([
-            new THREE.Vector3(-14, 0.0079, 0),
-            new THREE.Vector3(-9, 3, 0),
-            new THREE.Vector3(-4, 0, 0)
-        ]);
-
+        // Information to animate player three
+        this.playerThree = {
+            flipPath: new THREE.CatmullRomCurve3([
+                new THREE.Vector3(0, 0, -14),
+                new THREE.Vector3(0, 3, -9),
+                new THREE.Vector3(0, 0, -4)
+            ]),
+            index: -1,
+            stack: 0.01,
+            color: 0x00ff00
+        };
     }
 
+    // Will move the card and flip it 
     flipCard(player, object, time, war) {
+        // Which player are we moving the card for
         var player = this.#getPlayer(player);
-        const obPosition = object.position.clone();
+        if (player === undefined){
+            return false;
+        }
+        // if this is the first call of the funtion
         if (player.index === -1) {
+            // Set the start of the path to the objects location
             player.flipPath.points[0] = (object.position.clone());
+            // Line use for debugging 
             const geo = new THREE.BufferGeometry().setFromPoints(player.flipPath.getPoints(50));
             const mat = new THREE.LineBasicMaterial({ color: player.color });
             const line = new THREE.Line(geo, mat);
             this.scene.add(line);
         }
-
+        // Index used to track where were moving the object to (the percentage of the path)
         const currIndex = (time / 400 % 4) / 4;
+        // If the animation has not started to loop
         if (currIndex > player.index) {
+            // Get the vertex at the current index
             const position = player.flipPath.getPointAt(currIndex);
+            // Move the object to the current index position
             object.position.copy(position);
-            // object.translateX(-1.25);
+            // If were more than 25% into the line
             if (currIndex >= 0.25) {
-                object.rotation.z = ((-Math.PI * ((currIndex - 0.25) / 0.75)) * -1);
+                // Rotate the object (flipping)
+                // Math.PI * the percentage of the rest of the path
+                object.rotation.z = -Math.PI * ((currIndex - 0.25) / 0.75);
             }
+            // Updating the player index to the current index
             player.index = currIndex;
+            // Return treu to keep the animation going
             return true
         }
-
+        // Else when the aimation start to loop 
+        // Move object to the end of the line
+        object.position.copy(player.flipPath.getPointAt(1));
+        // Move object to the correct height of the stack
+        object.position.y = player.stack;
+        // Make sure the object is flipped correctly
         object.rotation.z = Math.PI;
+        // Reseting the player index
         player.index = -1;
-        if (war) {
-            player.stack += 0.0079;
-        } else {
-            player.stack = 0.0079 / 2;
-        }
+        // If theres a war increase the stack height else reset it
+        player.stack = war ? player.stack += 0.01 : 0.01;
+        // Retunr false to stop animation
         return false;
     }
 
-    #flipCardInterface(object, time, war, path, index, stack) {
-        if (index === -1) {
-            path = new THREE.CatmullRomCurve3([
-                object.position.clone(),
-                new THREE.Vector3(-9, 3, 0),
-                new THREE.Vector3(-4, 0, 0)
-            ]);
-            console.log(path.getPointAt(0));
-            const geo = new THREE.BufferGeometry().setFromPoints(path.getPoints(50));
-            const mat = new THREE.LineBasicMaterial({ color: 0xff0000 });
-            const line = new THREE.Line(geo, mat);
-            this.scene.add(line);
-        }
-
-        const currIndex = (time / 400 % 4) / 4;
-        if (currIndex > index) {
-            const position = path.getPointAt(currIndex);
-            object.position.copy(position);
-            // object.translateX(-1.25);
-            if (currIndex >= 0.25) {
-                object.rotation.z = ((-Math.PI * ((currIndex - 0.25) / 0.75)) * -1);
-            }
-            index = currIndex;
-            return true
-        }
-
-        object.rotation.z = Math.PI;
-        index = -1;
-        if (war) {
-            stack += 0.0079;
-        } else {
-            stack = 0.0079 / 2;
-        }
-        return false;
-    }
-
+    // Used to get the player that were doing the animation to
     #getPlayer(player) {
         switch (player) {
             case 'ONE':
-                return this.playerOne
+                return this.playerOne;
             case 'TWO':
-                return this.playerTwo
+                return this.playerTwo;
             case 'THREE':
-                break;
+                return this.playerThree;
             default:
-                console.log("Unknown Player Number: ", player);
+                console.log("ERROR! Unknown Player Number: ", player);
                 break;
         }
     }
-
-    #getFlipPath(player) {
-        switch (player) {
-            case 'ONE':
-                return this.flipCardPathOne
-            case 'TWO':
-                return this.flipCardPathTwo
-            case 'THREE':
-                break;
-            default:
-                console.log("Unknown Player Number: ", player);
-                break;
-        }
-    }
-
-    #getIndex(player) {
-        switch (player) {
-            case 'ONE':
-                return this.indexOne
-            case 'TWO':
-                return this.indexTwo
-            case 'THREE':
-                break;
-            default:
-                console.log("Unknown Player Number: ", player);
-                break;
-        }
-    }
-
-    #getStack(player) {
-        switch (player) {
-            case 'ONE':
-                return this.stackOne
-            case 'TWO':
-                return this.stackTwo
-            case 'THREE':
-                break;
-            default:
-                console.log("Unknown Player Number: ", player);
-                break;
-        }
-    }
-
 }
--- main.js
+++ main.js
@@ -11,109 +11,65 @@ class Main {
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.renderer.setClearColor(0x272727);
         this.renderer.setAnimationLoop((time) => this.animate(time));
-
         this.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000)
         this.scene.add(this.camera);
-        this.camera.position.set(0, 50, 50);
+        this.camera.position.set(0, 40, 50);
+        this.camera.lookAt(0,0,0);
         document.body.appendChild(this.renderer.domElement);
 
         const gridHelper = new THREE.GridHelper(50, 50);
         this.scene.add(gridHelper);
 
+        const axisHelper = new THREE.AxesHelper(5);
+        this.scene.add(axisHelper);
+
+        // Temporary table top
         const tableTopGeometry = new THREE.CircleGeometry(18, 40);
         const tableTopMaterial = new THREE.MeshBasicMaterial({ color: 0x808080, side: THREE.DoubleSide });
         const tableTop = new THREE.Mesh(tableTopGeometry, tableTopMaterial);
         tableTop.rotateX(Math.PI / 2);
         this.scene.add(tableTop);
 
-        const axisHelper = new THREE.AxesHelper(5);
-        this.scene.add(axisHelper);
-
-        // Add the custom geometry
-        const vertices = [
-            // front
-            { pos: [0, 0.0079, 0], color: [1.0, 0, 0] },
-            { pos: [2.5, 0.0079, 0], color: [1.0, 0, 0] },
-            { pos: [2.5, 0.0079, -3.5], color: [1.0, 0, 0] },
-            { pos: [0, 0.0079, -3.5], color: [1.0, 0, 0] },
-            { pos: [0, 0, 0], color: [1.0, 1.0, 1.0] },
-            { pos: [2.5, 0, 0], color: [1.0, 1.0, 1.0] },
-            { pos: [2.5, 0, -3.5], color: [1.0, 1.0, 1.0] },
-            { pos: [0, 0, -3.5], color: [1.0, 1.0, 1.0] }
-        ];
-        const positions = [];
-        const normals = [];
-        const uvs = [];
-        const colors = []
-        for (const vertex of vertices) {
-            positions.push(...vertex.pos);
-            // normals.push(...vertex.norm);
-            // uvs.push(...vertex.uv);
-            colors.push(...vertex.color);
-        }
-        // const geometry = new THREE.BufferGeometry();
-
-        // geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(positions), 3));
-        // geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(colors), 3));
-        // const indices = new Uint16Array([
-        //     0, 1, 2, 0, 2, 3,  // Front face
-        //     5, 4, 7, 5, 7, 6,  // Back face
-        //     3, 2, 6, 3, 6, 7,  // Top face
-        //     4, 5, 1, 4, 1, 0,  // Bottom face
-        //     5, 6, 2, 5, 2, 1,  // Right face
-        //     7, 4, 0, 7, 0, 3  // Left face
-        // ]);
-        // geometry.setIndex(new THREE.BufferAttribute(indices, 1));
-
+        // Temporary Cards
         const geometry = new THREE.BoxGeometry(2.5, 0.0079, 3.5);
-
         const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
         const material2 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+        const material3 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
         this.card = new THREE.Mesh(geometry, material);
         this.card2 = new THREE.Mesh(geometry, material2);
-        this.card.position.set(-14, 0, 0);
-        this.card.lookAt(0, 0, 0);
-        this.card.translateY(0.0079 / 2);
-        this.card2.position.set(14, 0, 0);
-        this.card2.lookAt(0, 0, 0);
-        this.card2.translateY(0.0079 / 2);
+        this.card3 = new THREE.Mesh(geometry, material3);
+        this.card.position.set(-14, 0.01 / 2, 0);
+        this.card.rotateY(-Math.PI / 2);
+        this.card2.position.set(14, 0.01, 0);
+        this.card2.rotateY(Math.PI / 2);
+        this.card3.position.set(0, 0.01 / 2, -14);
+        this.card3.rotateY(Math.PI);
         this.scene.add(this.card);
         this.scene.add(this.card2);
+        this.scene.add(this.card3);
 
-
-        this.path = new THREE.CatmullRomCurve3([
+        // Line to test paths
+        this.testPath = new THREE.CatmullRomCurve3([
             new THREE.Vector3(0, 0.0079, -14),
             new THREE.Vector3(0, 3, -9),
             new THREE.Vector3(0, 0, -4)
         ]);
-
-
-        const geo = new THREE.BufferGeometry().setFromPoints(this.path.getPoints(50));
+        const geo = new THREE.BufferGeometry().setFromPoints(this.testPath.getPoints(50));
         const mat = new THREE.LineBasicMaterial({ color: 0xffffff });
         const line = new THREE.Line(geo, mat);
-
         this.scene.add(line);
 
+        // Object to call different animations
         this.Animations = new Animations(this.scene);
 
-        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-
-        this.act = true;
-        this.act2 = true;
-        // this.act2 = true;
+        // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
         window.addEventListener('resize', () => this.onWindowResize(), false);
     }
 
     // Our animate function
     animate(time) {
-        this.controls.update();
-        if (this.act) {
-            this.act = this.Animations.flipCard('ONE', this.card, time, true);
-        }
-        if (this.act2) {
-            this.act2 = this.Animations.flipCard('TWO', this.card2, time);
-        }
+        // this.controls.update();
         this.renderer.render(this.scene, this.camera);
     }
 
