c54f45d15196cbd7d32eb568f8806b8f0a41a7bc
britomartinw <britomartinw@merrimack.edu>
2024-10-30 12:45:17 -0400

    Working on refactoring for easier use of animations calls

--- Animations.js
+++ Animations.js
@@ -4,34 +4,117 @@ import * as THREE from 'three';
  * Handles all animations
  */
 export class Animations {
-    constructor() {
-        this.flipCardPath = new THREE.CatmullRomCurve3([
-            new THREE.Vector3(0, 0.4, 0),
-            new THREE.Vector3(0, 5, -5),
-            new THREE.Vector3(0, 0, -10)
+
+    constructor(scene) {
+        this.scene = scene;
+        // Path to flip the cards of the first player
+        this.flipCardPathOne = new THREE.CatmullRomCurve3([
+            new THREE.Vector3(-14, 0.0079, 0),
+            new THREE.Vector3(-9, 3, 0),
+            new THREE.Vector3(-4, 0, 0)
         ]);
-        this.indexOne = -1
+        // Used to simulate the start and end of animation for player ONE
+        this.indexOne = -1;
+        // Thickness of the first players stacked flip cards for player ONE
+        this.stackOne = 0.0079 / 2;
+
+        // Path to flip the cards of the first player for player TWO
+        this.flipCardPathTwo = new THREE.CatmullRomCurve3([
+            new THREE.Vector3(14, 0.0079, 0),
+            new THREE.Vector3(9, 3, 0),
+            new THREE.Vector3(4, 0, 0)
+        ]);
+        // Used to simulate the start and end of animation for player TWO
+        this.indexTwo = -1;
+        // Thickness of the first players stacked flip cards for player TWO
+        this.stackTwo = 0.0079 / 2;
 
     }
 
-    flipCard(object, time) {
+    flipCard(player, object, time, war) {
+        switch (player) {
+            case 'ONE':
+                return this.#flipCardInterface(object, time, war, this.flipCardPathOne, this.indexOne, this.stackOne);
+            case 'TWO':
+                break;
+            case 'THREE':
+                break;
+            default:
+                console.log("Unknown Player Number: ", player);
+                break;
+        }
+    }
+
+    #flipCardOne(object, time, war) {
+        if (this.indexOne === -1) {
+            this.flipCardPathOne = new THREE.CatmullRomCurve3([
+                object.position.clone(),
+                new THREE.Vector3(-9, 3, 0),
+                new THREE.Vector3(-4, 0, 0)
+            ]);
+            console.log(this.flipCardPathOne.getPointAt(0));
+            const geo = new THREE.BufferGeometry().setFromPoints(this.flipCardPathOne.getPoints(50));
+            const mat = new THREE.LineBasicMaterial({ color: 0xff00ff });
+            const line = new THREE.Line(geo, mat);
+            this.scene.add(line);
+        }
+
         const currIndex = (time / 400 % 4) / 4;
         if (currIndex > this.indexOne) {
-            const position = this.flipCardPath.getPointAt(currIndex);
+            const position = this.flipCardPathOne.getPointAt(currIndex);
             object.position.copy(position);
             // object.translateX(-1.25);
             if (currIndex >= 0.25) {
                 object.rotation.z = ((-Math.PI * ((currIndex - 0.25) / 0.75)) * -1);
-
-                // this.cube.rotateZ((Math.PI * ((t - 0.25) / 0.75)) - this.cube.rotation.z);
             }
             this.indexOne = currIndex;
             return true
         }
+
         object.rotation.z = Math.PI;
         this.indexOne = -1;
+        if (war) {
+            this.stackOne += 0.0079;
+        } else {
+            this.stackOne = 0.0079 / 2;
+        }
         return false;
     }
 
+    #flipCardInterface(object, time, war, path, index, stack) {
+        if (index === -1) {
+            path = new THREE.CatmullRomCurve3([
+                object.position.clone(),
+                new THREE.Vector3(-9, 3, 0),
+                new THREE.Vector3(-4, 0, 0)
+            ]);
+            console.log(path.getPointAt(0));
+            const geo = new THREE.BufferGeometry().setFromPoints(path.getPoints(50));
+            const mat = new THREE.LineBasicMaterial({ color: 0xff0000 });
+            const line = new THREE.Line(geo, mat);
+            this.scene.add(line);
+        }
+
+        const currIndex = (time / 400 % 4) / 4;
+        if (currIndex > index) {
+            const position = path.getPointAt(currIndex);
+            object.position.copy(position);
+            // object.translateX(-1.25);
+            if (currIndex >= 0.25) {
+                object.rotation.z = ((-Math.PI * ((currIndex - 0.25) / 0.75)) * -1);
+            }
+            index = currIndex;
+            return true
+        }
+
+        object.rotation.z = Math.PI;
+        index = -1;
+        if (war) {
+            stack += 0.0079;
+        } else {
+            stack = 0.0079 / 2;
+        }
+        return false;
+    }
 
 }
--- main.js
+++ main.js
@@ -14,23 +14,28 @@ class Main {
 
         this.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000)
         this.scene.add(this.camera);
-        // this.camera.position.set(-50, 30, 25);
-        this.camera.position.set(-10, 20, 30);
+        this.camera.position.set(0, 50, 50);
         document.body.appendChild(this.renderer.domElement);
 
         const gridHelper = new THREE.GridHelper(50, 50);
-        // this.scene.add(gridHelper);
+        this.scene.add(gridHelper);
 
-        const axisHelper = new THREE.AxesHelper();
-        // this.scene.add(axisHelper);
+        const tableTopGeometry = new THREE.CircleGeometry(18, 40);
+        const tableTopMaterial = new THREE.MeshBasicMaterial({ color: 0x808080, side: THREE.DoubleSide });
+        const tableTop = new THREE.Mesh(tableTopGeometry, tableTopMaterial);
+        tableTop.rotateX(Math.PI / 2);
+        this.scene.add(tableTop);
+
+        const axisHelper = new THREE.AxesHelper(5);
+        this.scene.add(axisHelper);
 
         // Add the custom geometry
         const vertices = [
             // front
-            { pos: [0, 0.2, 0], color: [1.0, 0, 0] },
-            { pos: [2.5, 0.2, 0], color: [1.0, 0, 0] },
-            { pos: [2.5, 0.2, -3.5], color: [1.0, 0, 0] },
-            { pos: [0, 0.2, -3.5], color: [1.0, 0, 0] },
+            { pos: [0, 0.0079, 0], color: [1.0, 0, 0] },
+            { pos: [2.5, 0.0079, 0], color: [1.0, 0, 0] },
+            { pos: [2.5, 0.0079, -3.5], color: [1.0, 0, 0] },
+            { pos: [0, 0.0079, -3.5], color: [1.0, 0, 0] },
             { pos: [0, 0, 0], color: [1.0, 1.0, 1.0] },
             { pos: [2.5, 0, 0], color: [1.0, 1.0, 1.0] },
             { pos: [2.5, 0, -3.5], color: [1.0, 1.0, 1.0] },
@@ -46,50 +51,54 @@ class Main {
             // uvs.push(...vertex.uv);
             colors.push(...vertex.color);
         }
-        const geometry = new THREE.BufferGeometry();
+        // const geometry = new THREE.BufferGeometry();
 
+        // geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(positions), 3));
+        // geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(colors), 3));
+        // const indices = new Uint16Array([
+        //     0, 1, 2, 0, 2, 3,  // Front face
+        //     5, 4, 7, 5, 7, 6,  // Back face
+        //     3, 2, 6, 3, 6, 7,  // Top face
+        //     4, 5, 1, 4, 1, 0,  // Bottom face
+        //     5, 6, 2, 5, 2, 1,  // Right face
+        //     7, 4, 0, 7, 0, 3  // Left face
+        // ]);
+        // geometry.setIndex(new THREE.BufferAttribute(indices, 1));
 
-        geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(positions), 3));
-        // geometry.setAttribute('normal', new THREE.BufferAttribute(new Float32Array(normals), normalNumComponents));
-        // geometry.setAttribute('uv', new THREE.BufferAttribute(new Float32Array(uvs), uvNumComponents));
-        geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(colors), 3));
-        const indices = new Uint16Array([
-            0, 1, 2, 0, 2, 3,  // Front face
-            5, 4, 7, 5, 7, 6,  // Back face
-            3, 2, 6, 3, 6, 7,  // Top face
-            4, 5, 1, 4, 1, 0,  // Bottom face
-            5, 6, 2, 5, 2, 1,  // Right face
-            7, 4, 0, 7, 0, 3  // Left face
-        ]);
-        geometry.setIndex(new THREE.BufferAttribute(indices, 1));
+        const geometry = new THREE.BoxGeometry(2.5, 0.0079, 3.5);
 
-        // const geometry2 = new THREE.BoxGeometry(2.5, 0.2, 1);
-        const material2 = new THREE.MeshBasicMaterial({ vertexColors: true });
-        this.cube = new THREE.Mesh(geometry, material2);
-        this.scene.add(this.cube);
+        const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+        const material2 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+        this.card = new THREE.Mesh(geometry, material);
+        this.card2 = new THREE.Mesh(geometry, material2);
+        this.card.position.set(-14, 0, 0);
+        this.card.lookAt(0, 0, 0);
+        this.card.translateY(0.0079 / 2);
+        this.card2.translateY(0.0079 + 0.0079 / 2);
+        this.scene.add(this.card);
+        this.scene.add(this.card2);
 
 
         this.path = new THREE.CatmullRomCurve3([
-            new THREE.Vector3(0, 0.4, 0),
-            new THREE.Vector3(0, 5, -5),
-            new THREE.Vector3(0, 0, -10)
-
+            new THREE.Vector3(0, 0.0079, -14),
+            new THREE.Vector3(0, 3, -9),
+            new THREE.Vector3(0, 0, -4)
         ]);
 
 
-        // const geo = new THREE.BufferGeometry().setFromPoints(this.path.getPoints(50));
+        const geo = new THREE.BufferGeometry().setFromPoints(this.path.getPoints(50));
         const mat = new THREE.LineBasicMaterial({ color: 0xffffff });
-        // const line = new THREE.Line(geo, mat);
+        const line = new THREE.Line(geo, mat);
 
-        // this.scene.add(line);
+        this.scene.add(line);
 
-        this.Animations = new Animations();
+        this.Animations = new Animations(this.scene);
 
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-        this.pt = 0;
 
-        this.act = true;
-        this.test = 0;
+        // this.act = true;
+        this.act2 = true;
+        // this.act2 = true;
 
         window.addEventListener('resize', () => this.onWindowResize(), false);
     }
@@ -97,37 +106,13 @@ class Main {
     // Our animate function
     animate(time) {
         this.controls.update();
-        const t = (time / 400 % 4) / 4;
-
         if (this.act) {
-
-            this.act = this.Animations.flipCard(this.cube, time);
-            // console.log(this.act)
-
+            this.act = this.Animations.flipCard(this.card2, time, true);
+            this.act2 = !this.act;
         }
-        if (!this.act){
-            this.act = !this.act;
+        if (this.act2) {
+            this.act2 = this.Animations.flipCard("ONE", this.card, time);
         }
-        console.log(this.test);
-        // if (t > this.pt) {
-        //     const pos = this.path.getPointAt(t);
-        //     this.cube.position.copy(pos);
-        //     // this.cube.translateX(-1.25);
-        //     // this.cube.rotateZ(Math.PI);
-        //     if (t >= 0.25) {
-        //         this.cube.rotation.z = ((-Math.PI * ((t - 0.25) / 0.75)) * -1);
-
-        //         // this.cube.rotateZ((Math.PI * ((t - 0.25) / 0.75)) - this.cube.rotation.z);
-        //     }
-        //     this.pt = t;
-        // } else {
-        //     this.cube.rotation.z = Math.PI;
-        //     console.log("math.pi")
-        // }
-
-
-
-
         this.renderer.render(this.scene, this.camera);
     }
 
